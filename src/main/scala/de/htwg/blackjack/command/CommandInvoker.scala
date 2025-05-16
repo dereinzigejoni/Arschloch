d4586bb426682147e520b7d09e0c4f595e49e2e9
dereinzigejoni <jo351lin@htwg-konstanz.de>
2025-05-16 10:48:39 +0200

    Auto-Commit am 2025-05-16 10:48:39

--- src/main/scala/de/htwg/blackjack/command/CommandInvoker.scala
+++ src/main/scala/de/htwg/blackjack/command/CommandInvoker.scala
@@ -4,13 +4,9 @@ package de.htwg.blackjack.command
 import scala.util.Try
 import de.htwg.blackjack.controller.GameController
 import de.htwg.blackjack.model.GameState
-
-/** Verwaltet Ausführung, Undo und Redo von Commands auf einen Controller. */
 class CommandInvoker(controller: GameController) {
   private var undoStack: List[GameState] = Nil
   private var redoStack: List[GameState] = Nil
-
-  /** Führt aus, pusht alten State auf Undo-Stack und leert den Redo-Stack */
   def execute(cmd: Command): Try[GameState] = {
     val oldState = controller.getState
     cmd.execute(oldState).map { newState =>
@@ -20,8 +16,6 @@ class CommandInvoker(controller: GameController) {
       newState
     }
   }
-
-  /** Macht den letzten Schritt rückgängig, verschiebt aktuellen State auf Redo-Stack */
   def undo(): Option[GameState] = undoStack match {
     case prev :: rest =>
       val current = controller.getState
@@ -32,8 +26,6 @@ class CommandInvoker(controller: GameController) {
     case Nil =>
       None
   }
-
-  /** Stellt den zuletzt undone Schritt wieder her, verschiebt aktuellen State zurück auf Undo-Stack */
   def redo(): Option[GameState] = redoStack match {
     case next :: rest =>
       val current = controller.getState
