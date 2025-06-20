07227dea726d39cc0d9837b59972cbb624ca8c81
dereinzigejoni <jo351lin@htwg-konstanz.de>
2025-06-20 08:36:14 +0200

    Auto-Commit am 2025-06-20 08:36:14

--- src/main/scala/de/htwg/blackjack/strategy/ConservativeDealer.scala
+++ src/main/scala/de/htwg/blackjack/strategy/ConservativeDealer.scala
@@ -1,10 +1,7 @@
 package de.htwg.blackjack.strategy
-
 import de.htwg.blackjack.model.{DealerBust, Deck, Finished, Hand, Status}
 import de.htwg.blackjack.strategy.interfacE.DealerStrategy
-
 import scala.annotation.tailrec
-
 class ConservativeDealer extends DealerStrategy {
   @tailrec
   private def drawTill17(deck: Deck, hand: Hand): (Deck, Hand) = {
@@ -14,15 +11,9 @@ class ConservativeDealer extends DealerStrategy {
       drawTill17(d2, hand.add(c))
     }
   }
-
   def play(deck: Deck, dealerHand: Hand): (Deck, Hand, Status) = {
     val (d2, finalHand) = drawTill17(deck, dealerHand)
     val st = if (finalHand.isBust) DealerBust else Finished
     (d2, finalHand, st)
   }
 }
-
-
-
-
-
--- src/main/scala/de/htwg/blackjack/strategy/interfacE/DealerStrategy.scala
+++ src/main/scala/de/htwg/blackjack/strategy/interfacE/DealerStrategy.scala
@@ -1,7 +1,5 @@
 package de.htwg.blackjack.strategy.interfacE
-
 import de.htwg.blackjack.model.{Deck, Hand, Status}
-
 trait DealerStrategy {
   def play(deck:Deck,dealerHand: Hand): (Deck,Hand,Status)
 }
